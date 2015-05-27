845ddefb67091681f4f8137c1a936ef2e7f80108
Joost Sijm <joostsijm@gmail.com>
2015-05-27 12:49:04 +0200

    Afdeling

--- horizon/hrn/afdelingen.php
+++ horizon/hrn/afdelingen.php
@@ -1,5 +1,5 @@
-<option <?php if ($afdeling == 'eco') { ?>selected <?php }; ?>value="eco">Economie</option>
-<option <?php if ($afdeling == 'gez') { ?>selected <?php }; ?>value="gez">Gezondheids</option>
-<option <?php if ($afdeling == 'hend') { ?>selected <?php }; ?>value="hend">Handel &amp; Dienst</option>
+<option <?php if ($afdeling == 'eco') { ?>selected <?php }; ?>value="ECO">Economie</option>
+<option <?php if ($afdeling == 'gez') { ?>selected <?php }; ?>value="GEZ">Gezondheids</option>
+<option <?php if ($afdeling == 'hend') { ?>selected <?php }; ?>value="HenD">Handel &amp; Dienst</option>
 <option <?php if ($afdeling == 'tec') { ?>selected <?php }; ?>value="tec">techniek</option>
-<option <?php if ($afdeling == 'wel') { ?>selected <?php }; ?>value="wel">welzijn</option>
+<option <?php if ($afdeling == 'wel') { ?>selected <?php }; ?>value="Wel">welzijn</option>
