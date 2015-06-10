5deef1bc0969b47a44bf5d712cadef3711efb881
Joost Sijm <joostsijm@gmail.com>
2015-06-10 11:09:31 +0200

    Handel en dienst toegevoegd.
    Aleen op Hoorn

--- horizon/hrn/HenD/rooster.php
+++ horizon/hrn/HenD/rooster.php
@@ -1,3 +1,3 @@
 <?php
-echo "<iframe src='http://rooster.horizoncollege.nl/rstr/ECO/AMR/400-ECO/Roosters/c/" . $date . "/c000" . $klas . ".htm'>Rooster</iframe>";
+echo "<iframe src='http://rooster.horizoncollege.nl/rstr/H&D/HRN/Roosters/" . $date . "/c/c000" . $klas . ".htm'>Rooster</iframe>";
 ?>
